c3e39d0166b539fc2b34d3aa54facda7c3b124b3
toolong <thienlongphambao@gmail.com>
2023-04-12 20:50:07 +0700

    done

--- BAI-2/app.js
+++ BAI-2/app.js
@@ -1,14 +1,16 @@
 function numberOneTriangle(n) {
+  var n = parseInt(document.getElementById('number').value);
   if (n < 1 || n > 10) {
-    return;
-  }
-  for (let i = 1; i <= n; i++) {
+    document.getElementById('result').innerHTML =
+      'Bạn đã nhập sai, vui lòng nhập lại!';
+  } else {
     let row = '';
-    for (let j = 0; j < i; j++) {
-      row += '* ';
+    for (let i = 1; i <= n; i++) {
+      for (let j = 0; j < i; j++) {
+        row += '* ';
+      }
+      row += '<br>';
+      document.getElementById('result').innerHTML = row;
     }
-    document.write(`<div>${row}</div>`);
   }
 }
-
-numberOneTriangle(6);
